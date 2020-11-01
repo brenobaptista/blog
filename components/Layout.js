8f8e16f801c3c46d3c3579ac09a61b4fdd8d1ecd
Breno Baptista <brenomb07@hotmail.com>
2020-11-01 02:58:41 -0300

    Finished 'Assets, Metadata, and CSS'

---
+++ components/Layout.js
@@ -0,0 +1,72 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import styles from '../styles/Layout.module.css'
+import utilStyles from '../styles/Utils.module.css'
+
+const name = 'Breno Baptista'
+export const siteTitle = 'Next.js Sample Website'
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              src="/images/profile.jpg"
+              className={utilStyles.borderCircle}
+              alt={name}
+              width={128}
+              height={128}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  alt={name}
+                  width={96}
+                  height={96}
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  )
+}
