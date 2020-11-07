151ba9de141002ec8f017478b635c04b6a5e990b
Breno Baptista <brenomb07@hotmail.com>
2020-11-07 20:23:41 -0300

    Added ThemeProvider and GlobalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    line-height: 1.6;
+    font-size: 1.125rem;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    color: ${props => props.theme.colors.primary};
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  img {
+    max-width: 100%;
+    display: block;
+  }
+`
