90b0c5492902993cb9c6c79fac54076105820f83
Breno Baptista <brenobaptista@protonmail.com>
2020-11-22 22:08:06 -0300

    Added fileMock

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from '@jest/types'
 
 const config: Config.InitialOptions = {
-  setupFilesAfterEnv: ['./jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg|ttf)$': '<rootDir>/__mocks__/fileMock.ts'
+  }
 }
 
 export default config
