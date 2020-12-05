87ef339209df1d35e7a4b11632462536b104c948
Breno Baptista <brenobaptista@protonmail.com>
2020-12-05 01:06:10 -0300

    Fixed styles for posts

--- src/styles/pages/Post.ts
+++ src/styles/pages/Post.ts
@@ -3,7 +3,36 @@ import styled from 'styled-components'
 const Body = styled.div`
   p {
     text-align: justify;
-    text-indent: 1.25rem;
+  }
+
+  img {
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  blockquote {
+    margin-left: 0;
+    margin-right: 0;
+    padding-left: 20px;
+    border-left: 5px solid #2e3436;
+  }
+
+  code,
+  pre {
+    font-family: Fira Code, monospace;
+    background-color: #dddcdb;
+  }
+
+  pre {
+    padding: 25px 30px;
+    border-radius: 8px;
+    overflow: auto;
+  }
+
+  p > code {
+    padding: 3px 5px;
+    border-radius: 4px;
+    word-break: break-word;
   }
 `
 
