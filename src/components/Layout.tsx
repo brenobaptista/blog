8bac15830eac353ffb51d4b947ea0a56ac782cd6
Breno Baptista <brenobaptista@protonmail.com>
2020-12-26 12:20:52 -0300

    Added ToggleSwitch to Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import ToggleSwitch from './ToggleSwitch'
+
 import {
   Container,
-  Header,
+  HomeHeader,
   Separator,
   ShortBio,
   BackToHome
@@ -18,23 +21,29 @@ interface Props {
 const name = 'Breno Baptista'
 export const siteTitle = 'Breno Baptista'
 
-const Layout = ({ children, home }: Props): JSX.Element => (
-  <Container>
-    <Head>
-      <link rel='icon' href='/favicon.ico' />
-      <meta name='description' content="Breno Baptista's Portfolio" />
-      <meta
-        property='og:image'
-        content={`https://og-image.now.sh/${encodeURI(
-          siteTitle
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+const Layout = ({ children, home }: Props): JSX.Element => {
+  const [switchValue, setSwitchValue] = useState(false)
+
+  return (
+    <Container>
+      <Head>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content="Breno Baptista's Portfolio" />
+        <meta
+          property='og:image'
+          content={`https://og-image.now.sh/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
+      </Head>
+      <ToggleSwitch
+        isOn={switchValue}
+        handleToggle={() => setSwitchValue(!switchValue)}
       />
-      <meta name='og:title' content={siteTitle} />
-      <meta name='twitter:card' content='summary_large_image' />
-    </Head>
-    <Header>
       {home && (
-        <>
+        <HomeHeader>
           <Image
             src='/images/profile.jpg'
             alt={name}
@@ -43,38 +52,43 @@ const Layout = ({ children, home }: Props): JSX.Element => (
             priority
           />
           <h1>{name}</h1>
+        </HomeHeader>
+      )}
+      <main>{children}</main>
+      {!home && (
+        <>
+          <Separator />
+          <ShortBio>
+            <Image
+              src='/images/profile.jpg'
+              alt={name}
+              width={80}
+              height={80}
+            />
+            <p>
+              Breno Baptista is a full-stack developer at{' '}
+              <a
+                href='https://joinmassive.com/team'
+                target='_blank'
+                rel='noreferrer noopener'
+                aria-label='Massive'
+              >
+                Massive
+              </a>
+              , where he develops web apps to fight advertising, data selling
+              and privacy violations. He is interested in GNU/Linux, open-source
+              software, privacy and cybersecurity.
+            </p>
+          </ShortBio>
+          <BackToHome>
+            <Link href='/'>
+              <a>← Back to home</a>
+            </Link>
+          </BackToHome>
         </>
       )}
-    </Header>
-    <main>{children}</main>
-    {!home && (
-      <>
-        <Separator />
-        <ShortBio>
-          <Image src='/images/profile.jpg' alt={name} width={80} height={80} />
-          <p>
-            Breno Baptista is a full-stack developer at{' '}
-            <a
-              href='https://joinmassive.com/team'
-              target='_blank'
-              rel='noreferrer noopener'
-              aria-label='Massive'
-            >
-              Massive
-            </a>
-            , where he develops web apps to fight advertising, data selling and
-            privacy violations. He is interested in GNU/Linux, open-source
-            software, privacy and cybersecurity.
-          </p>
-        </ShortBio>
-        <BackToHome>
-          <Link href='/'>
-            <a>← Back to home</a>
-          </Link>
-        </BackToHome>
-      </>
-    )}
-  </Container>
-)
+    </Container>
+  )
+}
 
 export default Layout
--- src/styles/components/Layout.ts
+++ src/styles/components/Layout.ts
@@ -19,7 +19,7 @@ export const Container = styled.div`
   }
 `
 
-export const Header = styled.header`
+export const HomeHeader = styled.header`
   display: flex;
   flex-direction: column;
   align-items: center;
