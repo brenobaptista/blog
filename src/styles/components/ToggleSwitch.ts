0e139f60c4854ed0d6cdd490fb3021b0f9a87f0f
Breno Baptista <brenobaptista@protonmail.com>
2020-12-26 12:20:29 -0300

    Updated ToggleSwitch styles

--- src/styles/components/ToggleSwitch.ts
+++ src/styles/components/ToggleSwitch.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Checkbox = styled.input`
+  position: absolute;
   height: 0;
   width: 0;
   visibility: hidden;
@@ -14,17 +15,18 @@ export const Label = styled.label<LabelProps>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  position: relative;
+  margin-bottom: 32px;
   cursor: pointer;
-  width: 100px;
-  height: 50px;
+  border-radius: 65px;
+  width: 65px;
+  height: 32px;
   background: ${props => {
     const { isOn } = props
     const { primary, altText } = props.theme.colors
 
     return isOn ? primary : altText
   }};
-  border-radius: 100px;
-  position: relative;
   transition: background-color 0.2s;
 `
 
@@ -33,15 +35,15 @@ export const Button = styled.span`
   position: absolute;
   top: 2px;
   left: 2px;
-  width: 45px;
-  height: 45px;
-  border-radius: 45px;
+  border-radius: 28px;
+  width: 28px;
+  height: 28px;
   transition: 0.2s;
   background: ${props => props.theme.colors.background};
   box-shadow: 0 0 2px 0 rgba(10, 10, 10, 0.29);
 
   ${Label}:active & {
-    width: 60px;
+    width: 33px;
   }
 
   ${Checkbox}:checked + ${Label} & {
