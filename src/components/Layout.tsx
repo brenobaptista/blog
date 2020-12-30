24a8939929b484f8e2a9b8b7c6a69c940caf338c
Breno Baptista <brenobaptista@protonmail.com>
2020-12-30 07:31:59 -0300

    Updated metas on Head

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -41,12 +41,15 @@ const Layout = ({ children, home }: Props): JSX.Element => {
     <Container>
       <Head>
         <link rel='icon' href='/favicon.ico' />
-        <meta name='description' content="Breno Baptista's Portfolio" />
+        <meta
+          name='description'
+          content="I'm a full-stack developer based in Fortaleza, Brazil."
+        />
         <meta
           property='og:image'
-          content={`https://og-image.now.sh/${encodeURI(
+          content={`https://og-image.vercel.app/${encodeURI(
             siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fhyper-bw-logo.svg`}
         />
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
