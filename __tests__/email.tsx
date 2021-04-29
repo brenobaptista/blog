e82d54aeb96c37b31507a09df559aa373543bde4
Breno Baptista <brenobaptista@protonmail.com>
2021-04-28 22:21:13 -0300

    Updated test paths

--- __tests__/email.tsx
+++ __tests__/email.tsx
@@ -1,5 +1,5 @@
 import { render } from '../testUtils'
-import Email from '../src/components/icons/Email'
+import Email from '../src/components/svgs/Email'
 
 test('matches snapshot', () => {
   const { asFragment } = render(<Email width={40} height={40} />)
