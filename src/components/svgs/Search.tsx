30171a1f98d848a74763a5acd95ff9a3ed0faa9a
Breno Baptista <brenobaptista@protonmail.com>
2021-04-30 01:24:49 -0300

    Added Search icon

---
+++ src/components/svgs/Search.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  width: number
+  height: number
+}
+
+const Search = ({ width, height }: Props): JSX.Element => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    width={width}
+    height={height}
+  >
+    <title>Search icon</title>
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2}
+      d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+    />
+  </svg>
+)
+
+export default Search
