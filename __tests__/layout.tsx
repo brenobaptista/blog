031de8f8d2bcc637c6b8e54f13e5dec3b04b38da
Breno Baptista <brenobaptista@protonmail.com>
2021-05-21 14:31:26 -0300

    Updated tests and snapshots

--- __tests__/layout.tsx
+++ __tests__/layout.tsx
@@ -7,11 +7,12 @@ const renderWithContext = (ui, value) =>
 
 test('matches snapshot', () => {
   const toggleTheme = jest.fn()
+  const theme = { mode: 'dark' }
   const { asFragment } = renderWithContext(
     <Layout>
       <></>
     </Layout>,
-    toggleTheme
+    { theme, toggleTheme }
   )
 
   expect(asFragment()).toMatchSnapshot()
@@ -23,11 +24,12 @@ test('toggles Switch in dark mode', () => {
       '{"mode":"dark","colors":{"primary":"#bd93f9","text":"#eaeaea","altText":"#bababa","background":"#282a36","altBackground":" #44475a","selector":"#cccccc","selectorEdge":"#333333"}}'
   )
   const toggleTheme = jest.fn()
+  const theme = { mode: 'dark' }
   const { getByTestId } = renderWithContext(
     <Layout>
       <></>
     </Layout>,
-    toggleTheme
+    { theme, toggleTheme }
   )
 
   const checkbox = getByTestId('label-checkbox')
@@ -41,11 +43,12 @@ test('toggles Switch in light mode', () => {
       '{"mode":"light","colors":{"primary":"#586693","text":"#2e3436","altText":"#565656","background":"#f6f5f4","altBackground":" #e4e3e2","selector":"#333333","selectorEdge":"#cccccc"}}'
   )
   const toggleTheme = jest.fn()
+  const theme = { mode: 'light' }
   const { getByTestId } = renderWithContext(
     <Layout>
       <></>
     </Layout>,
-    toggleTheme
+    { theme, toggleTheme }
   )
 
   const checkbox = getByTestId('label-checkbox')
