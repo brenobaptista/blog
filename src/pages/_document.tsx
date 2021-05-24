a5c7a401eaf238d9034b1b487e636eb5adbcd389
Breno Baptista <brenobaptista@protonmail.com>
2021-05-23 23:39:22 -0300

    Used RealFaviconGenerator for favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,7 +43,38 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link rel='icon' href='/favicon.ico' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/static/favicons/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/static/favicons/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/static/favicons/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/static/favicons/site.webmanifest' />
+          <link
+            rel='mask-icon'
+            href='/static/favicons/safari-pinned-tab.svg'
+            color='#282a36'
+          />
+          <link rel='shortcut icon' href='/static/favicons/favicon.ico' />
+          <meta name='apple-mobile-web-app-title' content='Breno Baptista' />
+          <meta name='application-name' content='Breno Baptista' />
+          <meta name='msapplication-TileColor' content='#282a36' />
+          <meta
+            name='msapplication-config'
+            content='/static/favicons/browserconfig.xml'
+          />
+          <meta name='theme-color' content='#282a36' />
           <link
             rel='alternate'
             type='application/rss+xml'
