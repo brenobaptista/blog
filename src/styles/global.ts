19b0e8813e6d2eb1ebd37361701041dee8647639
Breno Baptista <brenobaptista@protonmail.com>
2021-05-27 10:09:09 -0300

    Added Inter font

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,21 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+  @font-face {
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 100 900;
+    src: url(/fonts/inter-var-latin.woff2) format('woff2');
+    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+      U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+      U+FEFF, U+FFFD;
+  }
+
   html,
   body {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Roboto, Oxygen,
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
 
