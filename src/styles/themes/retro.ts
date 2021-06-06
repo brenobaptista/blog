7d9813b2de248de84cb8b86c22701266f6d80cee
Breno Baptista <brenobaptista@protonmail.com>
2021-06-06 14:24:51 -0300

    Updated colors for retro theme

--- src/styles/themes/retro.ts
+++ src/styles/themes/retro.ts
@@ -1,14 +1,14 @@
 const retro = {
   mode: 'retro',
   colors: {
-    primary: '#586693',
-    text: '#2e3436',
-    altText: '#565656',
-    background: '#f6f5f4',
-    altBackground: ' #e4e3e2',
-    selector: '#333333',
-    selectorEdge: '#cccccc',
-    visited: '#3d4766'
+    primary: 'orange',
+    text: 'white',
+    altText: 'gray',
+    background: 'black',
+    altBackground: 'black',
+    selector: 'white',
+    selectorEdge: 'white',
+    visited: 'darkorange'
   }
 }
 
