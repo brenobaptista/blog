a8426026c7052bf940e35bcc1ab954a950fc3825
Breno Baptista <brenobaptista@protonmail.com>
2021-08-25 16:41:32 -0300

    Updated tests and snapshots

--- __tests__/monero.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '../testUtils'
-import Monero from '../src/components/svgs/Monero'
-
-test('matches snapshot', () => {
-  const { asFragment } = render(<Monero width={40} height={40} />)
-
-  expect(asFragment()).toMatchSnapshot()
-})
