7f03ad387698976fddd30c0ae0a7e0377e7ac315
Breno Baptista <brenobaptista@protonmail.com>
2021-09-30 01:43:36 -0300

    Update Jest and fixed issues

--- jest.config.ts
+++ jest.config.ts
@@ -6,15 +6,23 @@ const config: Config.InitialOptions = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg|ttf)$': '<rootDir>/__mocks__/fileMock.ts'
   },
-  collectCoverageFrom: ['**/*.{js,ts,tsx}', '!**/*.d.ts', '!jest.**.ts'],
+  collectCoverageFrom: ['**/*.{js,ts,tsx}', '!**/*.d.ts'],
   coveragePathIgnorePatterns: [
     '<rootDir>/.next/',
     '<rootDir>/coverage/',
     '<rootDir>/node_modules/',
     '<rootDir>/src/pages/_app.tsx',
     '<rootDir>/src/pages/_document.tsx',
-    '<rootDir>/src/styles/global.ts'
-  ]
+    '<rootDir>/src/styles/global.ts',
+    '<rootDir>/jest.config.ts',
+    '<rootDir>/jest.setup.ts',
+    '<rootDir>/next.config.js'
+  ],
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  transformIgnorePatterns: ['/node_modules/']
 }
 
 export default config
