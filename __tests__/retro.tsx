d476c49b4024ad7939171ec5fbfb309b2105916f
Breno Baptista <brenobaptista@protonmail.com>
2022-02-19 13:51:47 -0300

    Added tests for Retro component

---
+++ __tests__/retro.tsx
@@ -0,0 +1,13 @@
+import { render } from '../testUtils'
+import Retro from '../src/components/Retro'
+import ThemeContext from '../src/contexts/ThemeContext'
+
+const renderWithContext = (ui, value) =>
+  render(<ThemeContext.Provider value={value}>{ui}</ThemeContext.Provider>)
+
+test('matches snapshot', () => {
+  const theme = { mode: 'retro' }
+  const { asFragment } = renderWithContext(<Retro />, { theme })
+
+  expect(asFragment()).toMatchSnapshot()
+})
