9918ab8844728a9c6979513a76db10eae73db31d
Breno Baptista <brenobaptista@protonmail.com>
2022-10-20 20:30:49 -0300

    Ask to print the resume page automatically

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 import { Email, GitHub, RSS } from '../components/svgs'
 import {
@@ -11,142 +12,148 @@ import {
   Section
 } from '../styles/pages/Resume'
 
-const Resume = (): JSX.Element => (
-  <>
-    <Head>
-      <title>Résumé</title>
-      <meta
-        name='description'
-        content='My résumé page that can be exported into a PDF file.'
-      />
-    </Head>
-    <PrintingRules />
-    <Page>
-      <Content>
-        <Header>
-          <Author>
-            <h1>Breno Baptista</h1>
-            <div>Full-Stack Developer</div>
-          </Author>
-          <Social>
+const Resume = (): JSX.Element => {
+  useEffect(() => {
+    print()
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Résumé</title>
+        <meta
+          name='description'
+          content='My résumé page that can be exported into a PDF file.'
+        />
+      </Head>
+      <PrintingRules />
+      <Page>
+        <Content>
+          <Header>
+            <Author>
+              <h1>Breno Baptista</h1>
+              <div>Full-Stack Developer</div>
+            </Author>
+            <Social>
+              <div>
+                <a
+                  href='mailto:brenobaptista@protonmail.com'
+                  aria-label='Contact me by email'
+                >
+                  <Email width={16} height={16} />
+                  <span>brenobaptista@protomail.com</span>
+                </a>
+              </div>
+              <div>
+                <a
+                  href='https://www.brenobaptista.me/'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                  aria-label='My blog'
+                >
+                  <RSS width={16} height={16} />
+                  <span>brenobaptista.me</span>
+                </a>
+              </div>
+              <div>
+                <a
+                  href='https://github.com/brenobaptista/'
+                  target='_blank'
+                  rel='noreferrer noopener'
+                >
+                  <GitHub width={16} height={16} />
+                  <span>github.com/brenobaptista</span>
+                </a>
+              </div>
+            </Social>
+          </Header>
+          <Section>
+            <h2>EXPERIENCE</h2>
             <div>
-              <a
-                href='mailto:brenobaptista@protonmail.com'
-                aria-label='Contact me by email'
-              >
-                <Email width={16} height={16} />
-                <span>brenobaptista@protomail.com</span>
-              </a>
+              <strong>Software Engineer, Alternative</strong>
+              <em>February 2022 - Present (USA, remote)</em>
+              <ul>
+                <li>Built containerized microservices in Go.</li>
+                <li>Delighted clients by crafting web experiences.</li>
+              </ul>
             </div>
             <div>
-              <a
-                href='https://www.brenobaptista.me/'
-                target='_blank'
-                rel='noreferrer noopener'
-                aria-label='My blog'
-              >
-                <RSS width={16} height={16} />
-                <span>brenobaptista.me</span>
-              </a>
+              <strong>Jr. Software Engineer, Massive</strong>
+              <em>August 2020 - January 2022 (USA, remote)</em>
+              <ul>
+                <li>
+                  Fought intrusive advertising by developing desktop apps to
+                  connect partners.
+                </li>
+                <li>
+                  Led efforts to vastly reduce technical debt in systems,
+                  launching a refactored partner dashboard with better UI/UX and
+                  performance.
+                </li>
+              </ul>
             </div>
             <div>
-              <a
-                href='https://github.com/brenobaptista/'
-                target='_blank'
-                rel='noreferrer noopener'
-              >
-                <GitHub width={16} height={16} />
-                <span>github.com/brenobaptista</span>
-              </a>
+              <strong>Jr. Full-Stack Developer, Privacy Pixel</strong>
+              <em>January 2020 - August 2020 (USA, remote)</em>
+              <ul>
+                <li>
+                  Helped businesses comply with data laws (GDPR/CCPA) with our
+                  plugins.
+                </li>
+                <li>
+                  Developed internal tools to increase developer efficiency
+                  using Bash.
+                </li>
+              </ul>
             </div>
-          </Social>
-        </Header>
-        <Section>
-          <h2>EXPERIENCE</h2>
-          <div>
-            <strong>Software Engineer, Alternative</strong>
-            <em>February 2022 - Present (USA, remote)</em>
-            <ul>
-              <li>Built containerized microservices in Go.</li>
-              <li>Delighted clients by crafting web experiences.</li>
-            </ul>
-          </div>
-          <div>
-            <strong>Jr. Software Engineer, Massive</strong>
-            <em>August 2020 - January 2022 (USA, remote)</em>
-            <ul>
-              <li>
-                Fought intrusive advertising by developing desktop apps to
-                connect partners.
-              </li>
-              <li>
-                Led efforts to vastly reduce technical debt in systems,
-                launching a refactored partner dashboard with better UI/UX and
-                performance.
-              </li>
-            </ul>
-          </div>
-          <div>
-            <strong>Jr. Full-Stack Developer, Privacy Pixel</strong>
-            <em>January 2020 - August 2020 (USA, remote)</em>
-            <ul>
-              <li>
-                Helped businesses comply with data laws (GDPR/CCPA) with our
-                plugins.
-              </li>
-              <li>
-                Developed internal tools to increase developer efficiency using
-                Bash.
-              </li>
-            </ul>
-          </div>
-          <div>
-            <strong>Product Owner, Acens</strong>
-            <em>February 2019 - January 2020 (Brazil)</em>
-            <ul>
-              <li>
-                Volunteered to help businesses build an online presence by
-                managing projects.
-              </li>
-              <li>
-                Improved the organization productivity by enforcing agile across
-                the company.
-              </li>
-            </ul>
-          </div>
-        </Section>
-        <Section>
-          <h2>EDUCATION</h2>
-          <div>
-            <strong>Bachelor’s Degree in Computer Science</strong>
-            <div>Unifacvest University Center (2019 - 2023)</div>
-          </div>
-          <div>
-            <strong>Scrum Fundamentals Certified (SFC™)</strong>
             <div>
-              SCRUMstudy - Accreditation Body for Scrum and Agile (2020)
+              <strong>Product Owner, Acens</strong>
+              <em>February 2019 - January 2020 (Brazil)</em>
+              <ul>
+                <li>
+                  Volunteered to help businesses build an online presence by
+                  managing projects.
+                </li>
+                <li>
+                  Improved the organization productivity by enforcing agile
+                  across the company.
+                </li>
+              </ul>
             </div>
-          </div>
-        </Section>
-        <Section>
-          <h2>SKILLS</h2>
-          <div>
-            <b>Languages: </b>JavaScript, TypeScript, Go, Bash, Lua
-          </div>
-          <div>
-            <b>Front-end: </b>HTML, CSS, Sass, Styled Components, Tailwind,
-            React, React Native, Next.js, Accessibility, Jest/RTL, Cypress
-          </div>
-          <div>
-            <b>Back-end: </b>Node, Express, Deno, GraphQL, Postgres, MongoDB
-          </div>
-          <div>
-            <b>DevOps: </b>Git, Docker, AWS, GNU/Linux, Yarn Workspaces, CI/CD
-          </div>
-        </Section>
-      </Content>
-    </Page>
-  </>
-)
+          </Section>
+          <Section>
+            <h2>EDUCATION</h2>
+            <div>
+              <strong>Bachelor’s Degree in Computer Science</strong>
+              <div>Unifacvest University Center (2019 - 2023)</div>
+            </div>
+            <div>
+              <strong>Scrum Fundamentals Certified (SFC™)</strong>
+              <div>
+                SCRUMstudy - Accreditation Body for Scrum and Agile (2020)
+              </div>
+            </div>
+          </Section>
+          <Section>
+            <h2>SKILLS</h2>
+            <div>
+              <b>Languages: </b>JavaScript, TypeScript, Go, Bash, Lua
+            </div>
+            <div>
+              <b>Front-end: </b>HTML, CSS, Sass, Styled Components, Tailwind,
+              React, React Native, Next.js, Accessibility, Jest/RTL, Cypress
+            </div>
+            <div>
+              <b>Back-end: </b>Node, Express, Deno, GraphQL, Postgres, MongoDB
+            </div>
+            <div>
+              <b>DevOps: </b>Git, Docker, AWS, GNU/Linux, Yarn Workspaces, CI/CD
+            </div>
+          </Section>
+        </Content>
+      </Page>
+    </>
+  )
+}
 
 export default Resume
