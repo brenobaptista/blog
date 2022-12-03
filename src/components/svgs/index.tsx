1b1076c4a9f867c44a5fc5ced75372e85d78b8f8
Breno Baptista <brenobaptista@protonmail.com>
2022-12-03 14:52:13 -0300

    Add LinkedIn icon

--- src/components/svgs/index.tsx
+++ src/components/svgs/index.tsx
@@ -1,7 +1,8 @@
 import Email from './Email'
 import GitHub from './GitHub'
+import LinkedIn from './LinkedIn'
 import Magnifier from './Magnifier'
 import Portrait from './Portrait'
 import RSS from './RSS'
 
-export { Email, GitHub, Magnifier, Portrait, RSS }
+export { Email, GitHub, LinkedIn, Magnifier, Portrait, RSS }
