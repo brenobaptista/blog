fce5b0e2a0c3650f5ec507710efde2b9ecc19ab3
Breno Baptista <brenobaptista@protonmail.com>
2022-12-09 19:17:09 -0300

    Use @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
 
 import Theme from '../components/Theme'
 import GlobalStyle from '../styles/global'
 
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
   <Theme>
-    <Component {...pageProps} />
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
     <GlobalStyle />
   </Theme>
 )
