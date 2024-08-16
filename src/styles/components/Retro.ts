508d4a958f021be1fda3e84d2ed11bcd0f1ab4f3
Breno Baptista <47641641+brenobaptista@users.noreply.github.com>
2024-08-16 20:55:57 -0300

    Simplify blink animation

--- src/styles/components/Retro.ts
+++ src/styles/components/Retro.ts
@@ -47,14 +47,12 @@ export const Blink = styled.div`
   animation: blink 1s steps(1, end) infinite;
 
   @keyframes blink {
-    0% {
+    0%,
+    100% {
       opacity: 1;
     }
     50% {
       opacity: 0;
-    }
-    100% {
-      opacity: 1;
     }
   }
 `
