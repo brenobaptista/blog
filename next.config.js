efc23a36106d7b8a92799ecf7a08d18a5cbf7304
Breno Baptista <47641641+brenobaptista@users.noreply.github.com>
2026-04-30 22:09:57 -0300

    Remove headers

--- next.config.js
+++ next.config.js
@@ -7,48 +7,5 @@ module.exports = {
   devIndicators: false,
   compiler: {
     styledComponents: true
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders
-      }
-    ]
   }
 }
-
-const ContentSecurityPolicy = `
-  default-src 'self';
-  style-src 'self' 'unsafe-inline';
-  img-src * blob: data:;
-  connect-src *;
-  font-src 'self';
-`
-
-const securityHeaders = [
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, '')
-  },
-  {
-    key: 'Referrer-Policy',
-    value: 'strict-origin-when-cross-origin'
-  },
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff'
-  },
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on'
-  },
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload'
-  },
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()'
-  }
-]
